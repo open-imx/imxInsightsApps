36d04f1d4a5176ce0788007ccf798ec0e8d7d371
Hazedd <hazzdd@no-replay.github.com>
2025-06-09 22:29:42 +0200

    linter

--- imxInsightsApps/gui/ui/components.py
+++ imxInsightsApps/gui/ui/components.py
@@ -24,7 +24,10 @@ def create_situation_select(label: str) -> Select:
 
 
 def create_spinner_dialog():
-    with ui.dialog().props('persistent') as dialog, ui.card().classes("p-8 items-center"):
+    with (
+        ui.dialog().props("persistent") as dialog,
+        ui.card().classes("p-8 items-center"),
+    ):
         ui.spinner(size="lg").props("color=primary")
         ui.label("⏳ Processing, please wait... 🛠️").classes("mt-4")
     return dialog
